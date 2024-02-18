167e0a1e0358952062fe250c1e832aa3f5ed82c9
DAK Lights <github@daklights.com>
2024-02-17 20:26:23 -0500

    Reorder CEC

--- src/player.sh
+++ src/player.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# turn on TV
-#echo "on 0.0.0.0" | cec-client -s -d 1
-
-# set TV to this input
-#echo "as 0.0.0.0" | cec-client -s -d 1
-
 # start CVLC video
-/usr/bin/cvlc --fullscreen --no-audio --loop --no-video-title-show /home/pi/videos/ripples.mp4
+/usr/bin/cvlc --fullscreen --no-audio --loop --no-video-title-show /home/pi/videos/*.mp4
+
+# turn on TV (if supported)
+echo "on 0.0.0.0" | cec-client -s -d 1
+
+# set TV to this input (if supported)
+echo "as 0.0.0.0" | cec-client -s -d 1
